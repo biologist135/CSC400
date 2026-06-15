858386670929083d9d64646b96acfe03b2b020e3
Benjamin Platt <benjamin.platt@csuglobal.edu>
2026-06-14 18:50:44 -0700

    Added Comments

--- JavaBagDataStructure/src/bagPackage/Bag.java
+++ JavaBagDataStructure/src/bagPackage/Bag.java
@@ -3,28 +3,36 @@ package bagPackage;
 public class Bag<T> {
 	private Object[] bagArray;
 	
+	//Constructor creates an empty bag
 	public Bag() {
 		bagArray = new Object[0];
 		
 	}
-	
+	// Adds an item to the bag by creating a new array that is one space larger.
 	public void add(T item) {
 		Object[] tempArray = new Object[bagArray.length + 1];
 		
+		//Copies all current items into the temporary array.
 		for (int i = 0; i < bagArray.length; i++) {
 			tempArray[i] = bagArray[i];
 		}
+		
+		//Adds the new item to the array at the end.
 		tempArray[tempArray.length - 1] = item;
 		
+		//Replaces the old array with the new array.
 		bagArray = tempArray;
 	}
 	
+	//Removes one occurrence of the item from the bag, if it exists.
 	public void remove (T item) {
+		//Only attempts to remove the item if it exists
 		if (contains(item)) {
 			Object[] tempArray = new Object[bagArray.length -1];
 			int currIndex = 0;
 			boolean removed = false;
 			
+			//Copy every item except the first matching item
 			for (int i = 0; i < bagArray.length; i++) {
 				if(!removed && bagArray[i].equals(item)) {
 					removed = true;
@@ -34,9 +42,12 @@ public class Bag<T> {
 					currIndex++;
 				}
 			}
+			//Replaces the old array with the new array.
 			bagArray = tempArray;
 		}
 	}
+	
+	//Returns true if the item exists in the bag.
 	public boolean contains(T item) {
 		for (int i = 0; i < bagArray.length; i++) {
 			if (bagArray[i].equals(item)){
@@ -46,6 +57,7 @@ public class Bag<T> {
 		return false;
 	}
 	
+	//Counts how many times the specific item appear in the bag.
 	public int count(T item) {
 		int total = 0;
 		
@@ -56,6 +68,8 @@ public class Bag<T> {
 		}
 		return total;
 	}
+	
+	//Prints the contents of the bag.
 	public void printBag() {
 		System.out.println("The bag contains: ");
 		
--- JavaBagDataStructure/src/bagPackage/BagTest.java
+++ JavaBagDataStructure/src/bagPackage/BagTest.java
@@ -3,9 +3,11 @@ package bagPackage;
 public class BagTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Creates an instance of the bag class using string values
 		Bag<String> groceryBag = new Bag<>();
 		
+		//Adds several elements to the bag including duplicates "Apple" and "Banana"
 		groceryBag.add("Apple");
 		groceryBag.add("Banana");
 		groceryBag.add("Apple");
@@ -13,32 +15,40 @@ public class BagTest {
 		groceryBag.add("Banana");
 		groceryBag.add("Apple");
 		
-		System.out.println("Oribinal bag:");
+		//Prints the original bag.
+		System.out.println("Original bag:");
 		groceryBag.printBag();
 		
+		//Test the contains method.
 		System.out.println("Contains Apple? " + groceryBag.contains("Apple"));
 		System.out.println("Contains Grapes? " + groceryBag.contains("Grapes"));
 		System.out.println("Contains Orange? " + groceryBag.contains("Orange"));
 		
+		//Tests the count method.
 		System.out.println("Count Apple: " + groceryBag.count("Apple"));
 		System.out.println("Count Banana: "+ groceryBag.count("Banana"));
 		System.out.println("Count Grapes: "+ groceryBag.count("Grapes"));
 		
+		//Removes one apple.
 		System.out.println("\nRemoving one Apple.");
 		groceryBag.remove("Apple");
 		
+		//checks the bag after removing one apple.
 		System.out.println("\nBag after removing one Apple:");
 		groceryBag.printBag();
 		
+		//checks the contains method after removing one apple and also tests the count method.
 		System.out.println("\nContains Apple after removal? " + groceryBag.contains("Apple"));
 		System.out.println("Count Apples after removal: "+ groceryBag.count("Apple"));
 		
+		//Removes the only instance of orange.
 		System.out.println("\nRemoving Orange.");
 		groceryBag.remove("Orange");
 		
+		//Checks the bag after the removal of orange.
 		System.out.println("\nBag after removing one Orange: ");
 		groceryBag.printBag();
-		
+		//Checks the contains and count methods after removing the orange.
 		System.out.println("\nContains Orange after removal? " + groceryBag.contains("Orange"));
 		System.out.println("Count Orange after removal: "+ groceryBag.count("Orange"));
 			
