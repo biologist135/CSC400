9eab63f6341bf955f93ce3537fb21ea0f8c008bc
Benjamin Platt <benjamin.platt@csuglobal.edu>
2026-06-14 18:36:13 -0700

    Adding code for module 1

--- JavaBagDataStructure/src/bagPackage/BagTest.java
+++ JavaBagDataStructure/src/bagPackage/BagTest.java
@@ -4,4 +4,44 @@ public class BagTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Bag<S
+		Bag<String> groceryBag = new Bag<>();
+		
+		groceryBag.add("Apple");
+		groceryBag.add("Banana");
+		groceryBag.add("Apple");
+		groceryBag.add("Orange");
+		groceryBag.add("Banana");
+		groceryBag.add("Apple");
+		
+		System.out.println("Oribinal bag:");
+		groceryBag.printBag();
+		
+		System.out.println("Contains Apple? " + groceryBag.contains("Apple"));
+		System.out.println("Contains Grapes? " + groceryBag.contains("Grapes"));
+		System.out.println("Contains Orange? " + groceryBag.contains("Orange"));
+		
+		System.out.println("Count Apple: " + groceryBag.count("Apple"));
+		System.out.println("Count Banana: "+ groceryBag.count("Banana"));
+		System.out.println("Count Grapes: "+ groceryBag.count("Grapes"));
+		
+		System.out.println("\nRemoving one Apple.");
+		groceryBag.remove("Apple");
+		
+		System.out.println("\nBag after removing one Apple:");
+		groceryBag.printBag();
+		
+		System.out.println("\nContains Apple after removal? " + groceryBag.contains("Apple"));
+		System.out.println("Count Apples after removal: "+ groceryBag.count("Apple"));
+		
+		System.out.println("\nRemoving Orange.");
+		groceryBag.remove("Orange");
+		
+		System.out.println("\nBag after removing one Orange: ");
+		groceryBag.printBag();
+		
+		System.out.println("\nContains Orange after removal? " + groceryBag.contains("Orange"));
+		System.out.println("Count Orange after removal: "+ groceryBag.count("Orange"));
+			
+	}
+
+}
